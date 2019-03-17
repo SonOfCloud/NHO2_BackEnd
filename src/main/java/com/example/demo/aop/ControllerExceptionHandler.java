36fdd3864304620d947fb4b3c6a414e08ac40e31
chao.yang <chao.yang@thoughtworks.com>
2019-03-17 12:17:23 +0800

    add exception central handel

--- src/main/java/com/example/demo/aop/ControllerExceptionHandler.java
+++ src/main/java/com/example/demo/aop/ControllerExceptionHandler.java
@@ -1,13 +1,14 @@
 package com.example.demo.aop;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 @ControllerAdvice
@@ -17,12 +18,29 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    @ResponseStatus(value= HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String,Object> handlerUserNotExistException(Exception ex){
-        log.error("Exception detail:{}", ex.getMessage());
+    public Map<String,Object> handlerUserNotExistException(Exception ex, HttpServletResponse response){
+        log.error("Exception detail:{}", ex);
         Map<String,Object> result=new HashMap<>();
+        if(ex instanceof MethodArgumentNotValidException){
+            result.put("data", null);
+            result.put("error", argumentValidErrorMsg((MethodArgumentNotValidException) ex));
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return result;
+        }
         result.put("data", null);
         result.put("error", "Internal server error");
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         return result;
+    }
+
+    private String argumentValidErrorMsg(MethodArgumentNotValidException ex){
+        Iterator iterator = ex.getBindingResult().getAllErrors().iterator();
+
+        StringBuilder errorMsg = new StringBuilder();
+        while(iterator.hasNext()) {
+            ObjectError error = (ObjectError)iterator.next();
+            errorMsg.append("[").append(error.getDefaultMessage()).append("] ");
+        }
+        return errorMsg.toString();
     }
 }
